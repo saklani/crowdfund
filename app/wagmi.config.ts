ca87153e4605f64e5d35093af671f4329517e302
saklani <50768838+saklani@users.noreply.github.com>
2025-10-15 14:10:27 +0530

    feat: initialize components

---
+++ app/wagmi.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from '@wagmi/cli'
+import { foundry } from '@wagmi/cli/plugins'
+
+export default defineConfig({
+  out: 'lib/generated.ts',
+  contracts: [],
+  plugins: [
+    foundry({
+      project: '../contracts',
+    }),
+  ],
+})
